46e45344410efa3be1123c80ed762f09a6a12581
Yogi <yogeshcs2003@gmail.com>
2023-12-02 01:39:38 +0530

    .NET 8.0

---
+++ OcelotGateway/Program.cs
@@ -0,0 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+await app.UseOcelot();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
